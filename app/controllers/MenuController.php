152fd6233f7506d958ad43b1feb9373642c00b22
navoreee <nayra.jannatri@gmail.com>
2022-04-30 23:07:32 +0700

    manually added pictures for the categories

--- app/controllers/MenuController.php
+++ app/controllers/MenuController.php
@@ -1,10 +1,10 @@
 <?php
 
-class MenuController extends Controller {
+class MenuController extends Controller
+{
 
     public function index()
     {
-        $data['categories'] = $this->model('Category')->getAllCategories();
 
         $data['page_title'] = 'Menu';
 
@@ -16,23 +16,12 @@ class MenuController extends Controller {
     public function category($id)
     {
         $data['category'] = $this->model('Category')->getCategoryById($id);
-        $data['items'] = $this->model('MenuItem')->getItemsWhere('category_id','=',$id);
-        
+        $data['items'] = $this->model('MenuItem')->getItemsWhere('category_id', '=', $id);
+
         $data['page_title'] = 'Menu';
 
         $this->view('templates/header', $data);
         $this->view('menu/category', $data);
-        $this->view('templates/footer');
-    }
-
-    public function search($keyword)
-    {
-        $data['items'] = $this->model('MenuItem')->getItemsWhere('name','LIKE',"%$keyword%");
-
-        $data['page_title'] = 'Menu';
-
-        $this->view('templates/header', $data);
-        $this->view('menu/index', $data);
         $this->view('templates/footer');
     }
 }
