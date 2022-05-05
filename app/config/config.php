777039d86a5ee3100d1597d497b76ad2e545709f
navoreee <nayra.jannatri@gmail.com>
2022-05-05 12:58:28 +0700

    Added images for the menu items

--- app/config/config.php
+++ app/config/config.php
@@ -6,4 +6,4 @@ define('BASEURL', 'http://localhost/alam-bistro/public');
 define('DB_HOST', 'localhost');
 define('DB_USER', 'root');
 define('DB_PASS', '');
-define('DB_NAME', 'alambistro');
+define('DB_NAME', 'alambist_alambistro');
